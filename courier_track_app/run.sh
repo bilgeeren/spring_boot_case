b5ca08b017610b853bf663226d646bc46830b390
bilgeeren <bilgeeren97@gmail.con>
2020-09-30 22:36:28 +0300

    UPDATE: Run script updated

--- courier_track_app/run.sh
+++ courier_track_app/run.sh
@@ -1,3 +1,11 @@
-echo "Make sure you already have Maven on your PC"
-mvn install
-mvn spring-boot:run
+while getopts "m:j:" arg; do
+	case $arg in
+	m | *)
+		mvn install
+		mvn spring-boot:run
+		;;
+	j | * )
+		java -jar target/courier_track_app-0.0.1-SNAPSHOT.jar
+		;;
+	esac
+done
